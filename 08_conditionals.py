74c62420c2df082ba3fe9428af0f817ba2129428
RichRevo7 <ricardo.revolorio88@gmail.com>
2024-12-20 16:36:57 -0600

    corregion de letra en la condicionales

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -13,7 +13,7 @@ if my_condition == 10:
 if my_condition > 10 and my_condition < 20: 
     print('Es mayor que 10 y menor que 20')
 elif my_condition == 25:
-    print('ÇEs igual a 25')
+    print('Es igual a 25')
 else:
     print('Es menor o igual que 10 o mayor o igual que 20')
 
