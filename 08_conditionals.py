df6392299d3fe237d83190df2290a2f0e6f91513
RichRevo7 <ricardo.revolorio88@gmail.com>
2024-12-20 14:43:15 -0600

    se programo las condicionales y se creo loops

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -1,1 +1,28 @@
 ### Conditionals ###
+
+my_condition = False
+
+if my_condition: # es lo mismo que if my_condition == True:
+    print('Se ejecuta la condicion del if')
+
+my_condition = 5 * 3 
+
+if my_condition == 10: 
+    print('Se ejecuta la condicion del segundo if')
+
+if my_condition > 10 and my_condition < 20: 
+    print('Es mayor que 10 y menor que 20')
+elif my_condition == 25:
+    print('ÇEs igual a 25')
+else:
+    print('Es menor o igual que 10 o mayor o igual que 20')
+
+    print('La ejecucion continua')
+
+my_string = ''
+
+if not my_string:
+    print('Mi cadena de texto es vacia')
+
+if my_string == 'Micadena de textooooo':
+    print('Mis cadenas cadenas de texto coinciden')
