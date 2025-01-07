e61730dbf3f54c82d1a2963273815ce55c12163e
RichRevo7 <ricardo.revolorio88@gmail.com>
2025-01-06 21:44:33 -0600

    Se trabajo en Classes y se agrego Exceptions.py

--- 11_classes.py
+++ 11_classes.py
@@ -1,1 +1,29 @@
-### Classes
+### Classes ###
+
+class MyEmptyPerson:
+  pass
+
+print(MyEmptyPerson)
+print(MyEmptyPerson())
+
+class Person:
+    def __init__(self, name, surname, alias = 'Sin alias'):
+        self.full_name = f'{name} {surname} ({alias})' # Propiedad publica
+        self.__name = name # Propiedad privada
+    
+    def get_name (self):
+        return self.__name
+
+    def walk (self):
+        print(f'{self.full_name} Esta Caminando')
+
+my_person = Person('Ricardo', 'Revolorio')
+print(my_person.full_name)
+print(my_person.get_name())
+my_person.walk()
+
+my_other_person = Person('Ricardo', 'Revolorio', 'RichRevo')
+print(my_other_person.full_name)
+my_other_person.walk()
+my_other_person.full_name = 'Brais Moure (El loco de los perros)'
+print(my_other_person.full_name)
